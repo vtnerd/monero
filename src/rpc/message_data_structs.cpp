05bb61916abf35694a9d10585f29fe04e43617dc
Lee *!* Clagett <code@leeclagett.com>
2025-11-29 14:39:56 -0500

    Add missing file

---
+++ src/rpc/message_data_structs.cpp
@@ -0,0 +1,70 @@
+// Copyright (c) 2025, The Monero Project
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//    of conditions and the following disclaimer in the documentation and/or other
+//    materials provided with the distribution.
+// 
+// 3. Neither the name of the copyright holder nor the names of its contributors may be
+//    used to endorse or promote products derived from this software without specific
+//    prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "message_data_structs.h"
+#include "serialization/wire.h"
+#include "serialization/wire/adapted/vector.h"
+
+namespace cryptonote { namespace rpc
+{
+  namespace
+  {
+    template<typename F, typename T>
+    void map_block_blobs(F& format, T& self)
+    {
+      wire::object(format, WIRE_FIELD(block), WIRE_FIELD(transactions));
+    }
+  }
+  WIRE_DEFINE_OBJECT(block_with_transaction_blobs, map_block_blobs);
+
+  namespace
+  {
+    template<typename F, typename T>
+    void map_error(F& format, T& self)
+    {
+      wire::object(format, WIRE_FIELD(code), WIRE_FIELD(error_str), WIRE_FIELD(message));
+    }
+  }
+  WIRE_DEFINE_OBJECT(error, map_error);
+
+  namespace
+  {
+    template<typename F, typename T>
+    void map_output_distribution(F& format, T& self)
+    {
+      wire::object(format,
+        wire::field("distribution", std::ref(self.data.distribution)),
+        WIRE_FIELD(amount),
+        wire::field("start_height", std::ref(self.data.start_height)),
+        wire::field("base", std::ref(self.data.base)),
+        WIRE_FIELD(cumulative)
+      );
+    }
+  }
+  WIRE_DEFINE_OBJECT(output_distribution, map_output_distribution);
+}} // cryptonote // rpc
